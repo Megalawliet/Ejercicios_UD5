b8649a4522aab834ce6ee918d037666db37ca1f4
Noelia Maria Barrera Bedós <galowmere@gmail.com>
2021-02-04 22:57:19 +0100

    Reparar comentarios ej10

--- UD5/src/UD5/Ejercicio10.java
+++ UD5/src/UD5/Ejercicio10.java
@@ -12,18 +12,18 @@ public class Ejercicio10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Demanes cuantes ventes vols
 		Scanner lectura = new Scanner(System.in);
 		System.out.println("Cuántas ventas? ");
 		int numeroventas = lectura.nextInt();
 		int SumarVentas=0;
 		for(int i = 0; i<numeroventas; i++) {
-			//System.out.println("El numero de ventas es de: "+(i+1));
+			
 			int venta=lectura.nextInt();
 			SumarVentas=SumarVentas+venta;
 		}
 		//Demanes les ventes per pantalla
-		//String dialogo=JOptionPane.showInputDialog("Nº de ventas: ");
-		//int numero[]=new int[Integer.parseInt(dialogo)];
+		
 		System.out.println("El total es: " +SumarVentas);
 		
 		
