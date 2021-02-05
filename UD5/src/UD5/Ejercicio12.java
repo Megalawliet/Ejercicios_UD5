0ce4c4a9e3434c6a66fc3f573a28b565e1d6a291
Noelia Maria Barrera Bedós <galowmere@gmail.com>
2021-02-05 01:19:04 +0100

    Ej13 repared

--- UD5/src/UD5/Ejercicio12.java
+++ UD5/src/UD5/Ejercicio12.java
@@ -1,10 +1,22 @@
 package UD5;
-
+import javax.swing.JOptionPane;
 public class Ejercicio12 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i;
+		//int intentos = 4;
+		String pwd ="1234";
+		boolean ctrl = false;
+		
+		for(i=0;i<3 && !ctrl; i++) {
+			String contrasenya = JOptionPane.showInputDialog(null,"Password: ");
+			if (contrasenya.equals(pwd)) {
+				JOptionPane.showMessageDialog(null, "Enhorabuena!");
+				ctrl = true;
+			}
+			else {
+				//JOptionPane.showMessageDialog(null, "Error, quedan: " +(3-1)+" intentos");
+				JOptionPane.showMessageDialog(null, "Error, vuelve a intentarlo");	
+			}		
+		}		
 	}
-
 }
