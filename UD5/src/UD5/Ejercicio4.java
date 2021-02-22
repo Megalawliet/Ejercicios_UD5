9e13112bcaf4ebf43e695a3f80206d8f2af9ad97
Noelia Maria Barrera Bedós <galowmere@gmail.com>
2021-02-22 23:30:29 +0100

    extraño, pero deberia estar colgado desde hace semanas, no me percaté de que estaba para colgar, no se en qué punto no lo colgé. Lo siento, porque de estar hecho, ESTABA desde hace SEMANAS!

--- UD5/src/UD5/Ejercicio4.java
+++ UD5/src/UD5/Ejercicio4.java
@@ -1,24 +1,14 @@
 package UD5;
-
-//import java.util.Scanner;
-
 import javax.swing.JOptionPane;
 
-//import java.util.Scanner;
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//System.out.println();
-		//java.util.Scanner valor = new java.util.Scanner(System.in);
-		//Scanner leer = new Scanner(System.in);
 		String numero = JOptionPane.showInputDialog("Dime un radio: ");
 		double r = Double.parseDouble(numero);
-		//double a= 3.1416 * Math.pow(r*r); 
-		
-		
-		//double doble = Double.parseDouble(r);
+
 		double a= Math.PI*Math.pow(r, 2);
 		System.out.println("El area es: "+a);
 
